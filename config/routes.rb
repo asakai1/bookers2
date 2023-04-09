5e35e1ff200549d3cd830824b4c6c90eeff94b25
asakai1 <127031557+asakai1@users.noreply.github.com>
2023-04-09 06:28:51 +0000

    [Add]about

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :books, only: [:new, :index, :show]
   resources :users, only: [:index, :show, :edit]
 
-  get "/homes/about" => "homes#about", as: "about"
+  get "/homes/about" => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
