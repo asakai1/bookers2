27b15e6cea0c8075345cd9225b32ed1215bdedf0
asakai1 <127031557+asakai1@users.noreply.github.com>
2023-04-10 21:17:29 +0000

    [Fix]book_new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,12 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
-    @books = Book.all
+    
   end
 
   def create
     @book = Book.new(book_params)
+    @book.user_id = current_user.id
     if @book.save
       redirect_to book_path(@book.id)
     else
@@ -19,8 +20,10 @@ class BooksController < ApplicationController
   def show
   end
 
-def book_params
+  private
+
+  def book_params
     params.require(:book).permit(:title, :body)
-end
+  end
 
 end
